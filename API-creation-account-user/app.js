53c9b446fdf8ec673a0eec8fbea3b254fdfd44f3
Neto6391 <neto6391@gmail.com>
2019-02-25 15:01:52 -0300

    Create Data for test of Model and Finish Model Account

--- API-creation-account-user/app.js
+++ API-creation-account-user/app.js
@@ -3,13 +3,19 @@ import mongoose from "mongoose";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import Cryptr from "cryptr";
 
 //Import routes
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
 
 //Import Models
-import accountModel from "./models/accountModel";
+import AccountModel from "./models/accountModel";
+
+//Test data of account
+import dataAccount from "./data/account.json";
+
+const app = express();
 
 //Connection MongoDB
 mongoose.connect(process.env.MONGODB_URI);
@@ -17,7 +23,16 @@ const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Error of Connection"));
 
-let app = express();
+let securePassword = new Cryptr("aes256");
+let password = securePassword.encrypt(dataAccount.password);
+dataAccount.password = password;
+
+let account = new AccountModel(dataAccount);
+
+account.save(err => {
+	if (err) console.error("Error: " + err);
+	console.log("Account test@test.com created with success!");
+});
 
 app.use(logger("dev"));
 app.use(express.json());
--- API-creation-account-user/models/accountModel.js
+++ API-creation-account-user/models/accountModel.js
@@ -6,7 +6,38 @@ const autoIncrement = mongooseSequence(mongoose);
 
 const accountModel = new Schema(
 	{
-		_id: Number
+		_id: Number,
+		firstName: {
+			type: String,
+			required: true
+		},
+		lastName: {
+			type: String,
+			required: true
+		},
+		email: {
+			type: String,
+			required: true,
+			index: {
+				unique: true
+			}
+		},
+		password: {
+			type: String,
+			required: true
+		},
+		phoneNumber: {
+			type: Number,
+			required: true
+		},
+		dateBirth: {
+			type: Date,
+			required: true
+		},
+		gender: {
+			type: String,
+			required: true
+		}
 	},
 	{
 		_id: false
